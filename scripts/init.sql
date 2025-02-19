ad2100c53d5ba178f843b175846e96867d099df5
Jyle Oxcino <jyle.leon.oxcino@gmail.com>
2025-02-18 19:50:23 -0800

    final?

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE credentials;
 CREATE DATABASE uploads;
-CREATE USER 'upload_service'@'172.21.0.%' IDENTIFIED BY 'upload_service';
-CREATE USER 'stream_service'@'172.21.0.%' IDENTIFIED BY 'stream_service';
-GRANT ALL PRIVILEGES ON credentials.* TO 'upload_service'@'172.21.0.%';
-GRANT ALL PRIVILEGES ON uploads.* TO 'upload_service'@'172.21.0.%';
-GRANT ALL PRIVILEGES ON credentials.* TO 'stream_service'@'172.21.0.%';
-GRANT ALL PRIVILEGES ON uploads.* TO 'stream_service'@'172.21.0.%';
+CREATE USER 'upload_service'@'%' IDENTIFIED BY 'upload_service';
+CREATE USER 'stream_service'@'%' IDENTIFIED BY 'stream_service';
+GRANT ALL PRIVILEGES ON credentials.* TO 'upload_service'@'%';
+GRANT ALL PRIVILEGES ON uploads.* TO 'upload_service'@'%';
+GRANT ALL PRIVILEGES ON credentials.* TO 'stream_service'@'%';
+GRANT ALL PRIVILEGES ON uploads.* TO 'stream_service'@'%';
 FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS credentials.users (
